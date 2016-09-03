2ca58132b5667a72aa5775d33cd43c8ff1204029
harmangrewal <grewal.harman.1989@gmail.com>
2016-09-03 11:11:56 -0400

    project 1st commit

---
+++ src/tutorial2/Application.java
@@ -0,0 +1,28 @@
+package tutorial2;
+
+public class Application {
+
+	public static void main(String[] args) {
+       int myNumber = 88;
+       short myShort = 46;
+       long myLong =3000;
+       
+       double myDouble = 7.4343;
+       float myFloat = 346.7f;
+       
+       char myChar = 'y';
+       boolean myBoolean = false;
+       byte myByte = 127;
+
+       
+       System.out.println(myNumber);
+       System.out.println(myShort);
+       System.out.println(myLong);
+       System.out.println(myDouble);
+       System.out.println(myFloat);
+       System.out.println(myChar);
+       System.out.println(myBoolean);
+       System.out.println(myByte);
+	}
+
+}
